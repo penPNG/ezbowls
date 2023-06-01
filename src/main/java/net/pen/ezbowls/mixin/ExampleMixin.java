3ecd98df0cd1fab87d5ca7a5b36553c1f4f9b211
penPNG <graeme.roach01@yahoo.com>
2023-06-01 05:04:23 -0400

    they stack on their own

--- src/main/java/net/pen/ezbowls/mixin/ExampleMixin.java
+++ src/main/java/net/pen/ezbowls/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ezBowls.LOGGER.info("This line is printed by an example mod mixin!");
+		ezBowls.LOGGER.info("Your soup bowls stack on their own!");
 	}
 }
