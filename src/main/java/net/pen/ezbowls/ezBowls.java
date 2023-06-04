3978badfa9823bee2a91a277355f5c30e7d5a6f0
penPNG <graeme.roach01@yahoo.com>
2023-06-04 02:04:45 -0400

    removing some unused learning content

--- src/main/java/net/pen/ezbowls/ezBowls.java
+++ src/main/java/net/pen/ezbowls/ezBowls.java
@@ -1,34 +1,15 @@
 package net.pen.ezbowls;
 
 import net.fabricmc.api.ModInitializer;
-import net.pen.ezbowls.item.CustomItem;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ezBowls implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("EZBOWLS");
-
-	public static final CustomItem CUSTOM_ITEM =
-			Registry.register(Registry.ITEM, new Identifier("ezbowls", "custom_item"),
-			new CustomItem(new FabricItemSettings().group(ItemGroup.MISC)));
+	public static final Logger LOGGER = LoggerFactory.getLogger("ezbowls");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Your soup bowls stack on their own!");
-		assert CUSTOM_ITEM != null;
-		// IDEA was really upset that i didn't use this
-		// so i "used" it
 
 	}
 }
