95019a2306092183b62273634d8e478cfd98fcfa
penPNG <graeme.roach01@yahoo.com>
2023-06-04 01:54:53 -0400

    The rest of the mod

--- src/main/java/net/pen/ezbowls/mixin/StewStuffDoer.java
+++ src/main/java/net/pen/ezbowls/mixin/StewStuffDoer.java
@@ -7,9 +7,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.StewItem;
-import net.minecraft.text.Text;
+//import net.minecraft.text.Text;
 import net.minecraft.world.World;
-import net.pen.ezbowls.ezBowls;
+//import net.pen.ezbowls.ezBowls;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -20,14 +20,32 @@ public class StewStuffDoer extends Item {
         super(settings);
     }
 
-    // TODO make finding a bowl easy
-    public int getSlot(PlayerInventory inventory, Item item) {
-        return 0;
+    // Had to copy some code from the mc source because the genius devs made this ONE method private
+    public boolean canStackAddMore(ItemStack existingStack, ItemStack stack) {
+        return !existingStack.isEmpty() && ItemStack.canCombine(existingStack, stack) && existingStack.isStackable() && existingStack.getCount() < existingStack.getMaxCount() && existingStack.getCount() < 64;
     }
 
-    // TODO Make sure ItemStack in slot has less than 64 items
-    public boolean validSlot(int slot) {
-        return false;
+    // made finding a bowl easy
+    public int getValidSlot(PlayerInventory inventory, ItemStack stack) {
+        for (int i = 0; i < inventory.main.size(); ++i) {
+            if (!canStackAddMore(inventory.main.get(i), stack)) continue;
+            return i;
+        }
+        return -1;
+    }
+
+    // puts a bowl in one of a few predetermined empty slots or the last empty one
+    public int getLastEmptySlot(PlayerInventory inventory) {
+        if(inventory.getStack(8).isEmpty()) return 8; // returns even if the current selected stew/soup is in slot 8
+        if(inventory.getStack(17).isEmpty()) return 17; // end of the top row in the inventory
+
+        // finds an empty slot starting from the end of the inventory
+        for(int i = 35; i>=0; i--) {
+            if(inventory.getStack(i).isEmpty()) return i;
+        }
+
+        // if this returns, the game WILL crash
+        return -1;
     }
 
     /**
@@ -36,30 +54,36 @@ public class StewStuffDoer extends Item {
      */
     @Overwrite
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        if(!world.isClient) user.sendMessage(Text.literal(user.getName().getString() + " ate a stew!"));
+
+        // old pieces of code that are kinda neat
+        //if(!world.isClient) user.sendMessage(Text.literal(user.getName().getString() + " ate a stew!"));
         // if(!world.isClient) user.damage(DamageSource.FALL, 19);
 
         ItemStack itemStack = super.finishUsing(stack, world, user);
 
-        // TODO Use getSlot to find bowl and add 1 to it if validSlot returns true
-        PlayerInventory inventory = ((PlayerEntity)user).getInventory();
-        ItemStack[] leaves = new ItemStack[41];
-        if(!world.isClient) {
-            for (int i = 0; i < 41; i++) {
-                leaves[i] = new ItemStack(Items.ACACIA_LEAVES);
-                leaves[i].setCount(i+1);
-                inventory.setStack(i, leaves[i]);
-            }
-        }
-        ezBowls.LOGGER.info(String.valueOf(inventory.isEmpty()));
-
-        ezBowls.LOGGER.info(String.valueOf(Text.literal(inventory.toString())));
-
+        // quickly end if player is in creative mode, saves like 3 nanoseconds probably
         if(((PlayerEntity)user).getAbilities().creativeMode)
             return itemStack;
 
-        // TODO remove ItemStack if a slot already containing bowls exists and isn't full
-        return new ItemStack(Items.ITEM_FRAME);
+        PlayerInventory inventory = ((PlayerEntity)user).getInventory();
+        ItemStack bowl = new ItemStack(Items.BOWL);
+
+        // find some available slots to put a bowl in
+        int availableBowlSlot = (getValidSlot(inventory, bowl));
+        int availableEmptySlot = (getLastEmptySlot(inventory));
+
+        // There's a specific clause that doesn't work, so there are three cases
+        if(availableBowlSlot != -1) {
+            inventory.getStack(availableBowlSlot).increment(1);
+            return ItemStack.EMPTY;
+        } else if(inventory.selectedSlot!=8){ // i hate this bug, kinda doesn't make sense
+            // availableEmptySlot should see that slot 8 is already full of the stew/soup, but it doesn't
+            inventory.setStack(availableEmptySlot, new ItemStack(Items.BOWL));
+            return ItemStack.EMPTY;
+        }
+
+        // This only occurs if the player has the bowl in the last item slot because of how the method works
+        return new ItemStack(Items.BOWL);
     }
 
 }
